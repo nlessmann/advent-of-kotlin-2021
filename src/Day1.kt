d402a6ac6f1d841474f0d7441c6d3717051ae9ab
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-12-05 19:16:33 +0100

    Solution for day 1

---
+++ src/Day1.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+fun countIncreases(values: List<Int>, windowSize: Int): Int {
+    return values.windowed(windowSize).count { it.last() > it.first() }
+}
+
+fun main() {
+    val values = File("inputs", "day1.txt").readLines().map { it.trim().toInt() }
+
+    println("Solution 1: ${countIncreases(values, 2)}")
+    println("Solution 2: ${countIncreases(values, 4)}")
+}
