67c91abecf4e72787e1dd7823e12f4ff8c05ff04
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-12-07 12:01:06 +0100

    Better method name

--- src/Day7.kt
+++ src/Day7.kt
@@ -8,17 +8,17 @@ class CrabSwarm(initialPositions: List<Int>) {
     fun computeMinimalFuelCost(): Int {
         // We are looking for the geometric median as that minimizes the sum of distances
         val medianIndex = positions.size / 2
-
         var medianPosition = positions[medianIndex]
         if (positions.size % 2 == 0) {
-            val realMedian = (medianPosition + positions[medianIndex - 1]).toDouble() / 2
-            medianPosition = realMedian.roundToInt()
+            medianPosition = (
+                (medianPosition + positions[medianIndex - 1]) / 2.0
+            ).roundToInt()
         }
 
         return positions.sumOf { position -> (medianPosition - position).absoluteValue }
     }
 
-    fun computeWeightedMinimalFuelCost(searchWindow: Int = 10): Int {
+    fun computeMinimalWeightedFuelCost(searchWindow: Int = 10): Int {
         // We are looking for a position near the centroid as that minimizes the sum of
         // squared distances, which is about what we need
         val centroid = positions.average().roundToInt()
@@ -38,5 +38,5 @@ fun main() {
     val positions = File("inputs", "day7.txt").readText().split(",").map { it.toInt() }
     val swarm = CrabSwarm(positions)
     println("Solution 1: ${swarm.computeMinimalFuelCost()}")
-    println("Solution 2: ${swarm.computeWeightedMinimalFuelCost()}")
+    println("Solution 2: ${swarm.computeMinimalWeightedFuelCost()}")
 }
