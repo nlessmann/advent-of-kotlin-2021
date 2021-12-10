70b0875ce9e1f235630a237decee832f36a0e750
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-12-10 12:01:04 +0100

    Update Day10.kt

--- src/Day10.kt
+++ src/Day10.kt
@@ -17,19 +17,21 @@ class NavigationInstruction(line: String) {
 
     init {
         // Remove chunks until no more chunks can be removed, from the remaining
-        // characters we can tell whether the line was corrupted or incomplete
-        var previousLine: String
+        // characters we can tell whether the line is corrupted or incomplete
+        var previousVersion: String
         var reducedLine = line
 
         do {
-            previousLine = reducedLine
-            reducedLine = previousLine
+            previousVersion = reducedLine
+            reducedLine = previousVersion
                 .replace("()", "")
                 .replace("[]", "")
                 .replace("{}", "")
                 .replace("<>", "")
-        } while (reducedLine != previousLine && reducedLine.isNotBlank())
+        } while (reducedLine != previousVersion && reducedLine.isNotBlank())
 
+        // Compute corruption score based on first remaining closing character
+        // (if there is none, the result will be null and the score 0)
         corruptionScore = when (closingCharacter.find(reducedLine)?.value) {
             ")" -> 3
             "]" -> 57
@@ -38,11 +40,12 @@ class NavigationInstruction(line: String) {
             else -> 0
         }
 
-        completionScore = if (corrupted || reducedLine.isBlank()) {
-            // Line was either corrupted or not incomplete
+        completionScore = if (reducedLine.isBlank() || corrupted) {
+            // Line was either completely fine, or corrupted
             0
         } else {
-            // Line is not corrupted, but incomplete
+            // Line is not corrupted, but incomplete - missing closing characters are
+            // the opening characters in reverse order, use that to compute the score
             reducedLine.reversed().fold(0) { score, openingCharacter ->
                 score * 5 + when (openingCharacter) {
                     '(' -> 1
@@ -58,6 +61,7 @@ class NavigationInstruction(line: String) {
 
 fun main() {
     val instructions  = File("inputs", "day10.txt").readLines().map { NavigationInstruction(it) }
+
     val syntaxErrorScore = instructions.sumOf { it.corruptionScore }
     println("Solution 1: $syntaxErrorScore")
 
