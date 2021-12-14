ccd80675efbfaef9fc6edb001234e7280052e174
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-12-14 11:06:46 +0100

    Update Day14.kt

--- src/Day14.kt
+++ src/Day14.kt
@@ -23,7 +23,7 @@ class Polymer(filename: String) {
         }.toMutableMap()
 
         // Evolve pairs of 2 characters
-        elements.windowed(2).map {
+        elements.windowed(2).forEach {
            evolvePair(it[0] to it[1], steps).forEach { (c, n) ->
                counts[c] = (counts[c] ?: 0) + n
            }
@@ -37,8 +37,7 @@ class Polymer(filename: String) {
         // Two characters and remaining evolution steps are a unique identifier
         val evolution = Evolution(chars, steps)
 
-        // Check whether this combination is still missing from the lookup table
-        return lut.getOrPut(evolution) {
+        if (evolution !in lut) {
             // Look up which additional character we need to insert
             val additionalChar = pairInsertionRules[chars]
                 ?: throw IllegalArgumentException("Illegal character")
@@ -49,14 +48,16 @@ class Polymer(filename: String) {
             // If there are more steps to be done, create two new pairs and continue
             if (steps > 1) {
                 for (pair in listOf(chars.first to additionalChar, additionalChar to chars.second)) {
-                    evolvePair(pair, steps - 1).forEach {
-                        counts[it.key] = (counts[it.key] ?: 0) + it.value
+                    evolvePair(pair, steps - 1).forEach { (c, n) ->
+                        counts[c] = (counts[c] ?: 0) + n
                     }
                 }
             }
 
-            return counts
+            lut[evolution] = counts
         }
+
+        return lut[evolution] ?: throw IllegalStateException()
     }
 }
 
